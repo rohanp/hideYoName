246870f2dcc01d5e5787a54da9acc07ccf3d9f05
Rohan Pandit <pandit.rohan@gmail.com>
2014-11-02 15:18:59 -0500

    initial upload- supports reddit and tumblr

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("hiding your name!");
+$('.user').css("visibility", "hidden");
+$('div.open_blog').css("visibility", "hidden");
+$('.is_mine').not("#new_post").css("visibility","hidden");
