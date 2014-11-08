099435ddbe329b6596eda75800f65987f392e923
Rohan Pandit <pandit.rohan@gmail.com>
2014-11-07 22:48:00 -0500

    added options page

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 console.log("hiding your name!");
-$('.user').css("visibility", "hidden");
-$('div.open_blog').css("visibility", "hidden");
-$('.is_mine').not("#new_post").css("visibility","hidden");
+chrome.storage.sync.get("hideReddit", function(obj){
+	console.log(obj);
+});
+//Reddit
+chrome.storage.sync.get({
+        "hideReddit": true,
+        "hideTumblr": true,
+        "hideTwitter": true
+    }, function(items) {
+    	//Reddit
+    	if(items.hideReddit){
+			$('.user').css("visibility", "hidden");
+			$('.user a').attr('href','nice try!');
+		}
+		//Tumblr
+		if(items.hideTumblr){
+			$('div.open_blog').css("visibility", "hidden");
+			$('.is_mine').not("#new_post").css("visibility","hidden");
+		}
+
+		//Twitter
+		if(items.hideTwitter){
+			$('.u-linkComplex-target').css('visibility', 'hidden');
+			$('.my-tweet').css('visibility', 'hidden');
+		}
+	}
+);
